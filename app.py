24dce78cf405bffa7d4d1ead546f9d443bc103d7
Matt Sharp <matthew.sharp3@gmail.com>
2021-06-11 07:35:42 -0700

    Cleaned up windvane function and display module.

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-from weather_data import get_conditions, get_forcast
+import display
+from PIL import Image
 
-print(get_conditions()['temp'])
+# define image (pHat display size)
+width = 250
+height = 122
+img = Image.new(mode='RGB', size=(width, height), color=(0, 0, 0))
+
+display.windvane(img)
+
+img.show()
+img.close()
--- display.py
+++ display.py
@@ -1,13 +1,14 @@
 from PIL import Image, ImageDraw, ImageFont
 from weather_data import get_weather
 
-width = 250
-height = 122
-img = Image.new(mode='RGB', size=(width, height), color=(0, 0, 0))
-draw = ImageDraw.Draw(im=img, mode='RGB')
 
+def windvane(img):
+    """
+    Draws wind data onto 'img' object. Left side shows text data, right side shows direction on compass rose
+    """
+    # create image draw object
+    draw = ImageDraw.Draw(im=img, mode='RGB')
 
-def windvane():
     # get wind data
     conditions = get_weather()
     windspd = conditions['windSpeed']
@@ -17,23 +18,21 @@ def windvane():
     day = conditions['day']
     date = conditions['date']
 
-    print(winddir, windspd, windgust)
+    # Wind text
     wind = f"{day}, {date}\n \nWind Speed: {windspd}mph \nGusting: {windgust}mph \nPressure: {pressure}in"
-
     draw.text(xy=(24, 24), text=wind)
 
     # correct for draw start angle ()
     winddir -= 90
 
-    # def screen areas
+    # def screen area
     screenright = [(149, 24), (225, 100)]
 
     # cardinal direction lines
     draw.line(xy=[(187, 20), (187, 104)], fill=(255, 255, 255,), width=1)
     draw.line(xy=[(145, 62), (229, 62)], fill=(255, 255, 255,), width=1)
 
-    # Direction indicators
-    # font = ImageFont.load()
+    # Cardinal directions
     draw.text(xy=(185, 7), text='N', fill=(255, 255, 0))
     draw.text(xy=(185, 105), text='S', fill=(255, 255, 255))
     draw.text(xy=(235, 57), text='E', fill=(255, 255, 255))
@@ -46,12 +45,3 @@ def windvane():
     # wind direction
     draw.pieslice(xy=screenright, start=(winddir - 15), end=(winddir + 15),
                   fill=(255, 255, 0), outline=(255, 255, 0))
-    """
-    Overlays wind direction onto a compass rose displayed on right half of screen
-    """
-
-
-windvane()
-
-img.show()
-img.close()
