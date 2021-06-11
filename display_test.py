ffb747d9be11960ecc0509675da2a653062a22c3
Matt Sharp <matthew.sharp3@gmail.com>
2021-06-10 22:50:44 -0700

    Improved wind display and included text data.

--- display_test.py
+++ display_test.py
@@ -1,21 +1,57 @@
-from PIL import Image, ImageDraw
-from weather_data import get_conditions
+from PIL import Image, ImageDraw, ImageFont
+from weather_data import get_weather
 
 width = 250
 height = 122
-xy = [(50, 30), (200, 90)]
-
-img = Image.new(mode='RGB', size=(width, height), color=(255, 255, 255))
-
+img = Image.new(mode='RGB', size=(width, height), color=(0, 0, 0))
 draw = ImageDraw.Draw(im=img, mode='RGB')
 
-# draw.line(xy=xy, fill=(0, 0, 0), width=2)
+
+def windvane():
+    # get wind data
+    conditions = get_weather()
+    windspd = conditions['windSpeed']
+    windgust = conditions['windGust']
+    winddir = conditions['windDir']
+    pressure = conditions['pressure']
+    day = conditions['day']
+    date = conditions['date']
+
+    print(winddir, windspd, windgust)
+    wind = f"{day}, {date}\n \nWind Speed: {windspd}mph \nGusting: {windgust}mph \nPressure: {pressure}in"
+
+    draw.text(xy=(24, 24), text=wind)
+
+    # correct for draw start angle ()
+    winddir -= 90
+
+    # def screen areas
+    screenright = [(149, 24), (225, 100)]
+
+    # cardinal direction lines
+    draw.line(xy=[(187, 20), (187, 104)], fill=(255, 255, 255,), width=1)
+    draw.line(xy=[(145, 62), (229, 62)], fill=(255, 255, 255,), width=1)
+
+    # Direction indicators
+    # font = ImageFont.load()
+    draw.text(xy=(185, 7), text='N', fill=(255, 255, 0))
+    draw.text(xy=(185, 105), text='S', fill=(255, 255, 255))
+    draw.text(xy=(235, 57), text='E', fill=(255, 255, 255))
+    draw.text(xy=(135, 57), text='W', fill=(255, 255, 255))
+
+    # compass perimeter
+    draw.arc(xy=screenright, start=(winddir + 15),
+             end=(winddir - 15), fill=(255, 255, 255))
+
+    # wind direction
+    draw.pieslice(xy=screenright, start=(winddir - 15), end=(winddir + 15),
+                  fill=(255, 255, 0), outline=(255, 255, 0))
+    """
+    Overlays wind direction onto a compass rose displayed on right half of screen
+    """
 
 
-windDir = get_conditions()['windDir']
-print(windDir)
-
+windvane()
 
 img.show()
-# img.save('testimg.png')
 img.close()
--- weather_data.py
+++ weather_data.py
@@ -3,13 +3,21 @@ import requests
 import config
 
 
-def get_conditions():
+def get_weather():
 
     url = "https://api.weather.com/v2/pws/observations/current?stationId=" + \
         config.STATION+"&format=json&units=e&apiKey="+config.APIKEY
     response = requests.get(url)
 
     current_conditions = json.loads(response.text)
+    # print(current_conditions)
+
+    url = "https://api.weather.com/v3/wx/forecast/daily/5day?postalKey=" + \
+        config.ZIPCODE+":US&units=e&language=en-US&format=json&apiKey="+config.APIKEY
+    response = requests.get(url)
+
+    forecast = json.loads(response.text)
+    # print(forecast)
 
     conditions = {'temp': current_conditions['observations'][0]['imperial']['temp'],
                   'humidity': current_conditions['observations'][0]['humidity'],
@@ -19,16 +27,8 @@ def get_conditions():
                   'windDir': current_conditions['observations'][0]['winddir'],
                   'pressure': current_conditions['observations'][0]['imperial']['pressure'],
                   'precipRate': current_conditions['observations'][0]['imperial']['precipRate'],
-                  'precipTotal': current_conditions['observations'][0]['imperial']['precipTotal']}
+                  'precipTotal': current_conditions['observations'][0]['imperial']['precipTotal'],
+                  'date': (current_conditions['observations'][0]['obsTimeLocal'].split(' ')[0].split('-', 1)[1]),
+                  'day': forecast['dayOfWeek'][0]}
 
     return(conditions)
-
-
-def get_forcast():
-    url = "https://api.weather.com/v3/wx/forecast/daily/5day?postalKey=" + \
-        config.ZIPCODE+":US&units=e&language=en-US&format=json&apiKey="+config.APIKEY
-    response = requests.get(url)
-
-    forecast = json.loads(response.text)
-
-    return(forecast)
