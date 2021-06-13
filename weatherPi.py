5cd2f31ddf559334babb898250e43c619209b925
Matt Sharp <matthew.sharp3@gmail.com>
2021-06-13 14:41:55 -0700

    change to use inky.auto

--- weatherPi.py
+++ weatherPi.py
@@ -5,8 +5,12 @@ INKY library cannot be installed on other OS.
 
 from PIL import Image
 import display
-from inky import InkyPHAT
+from inky.auto import auto
 
-inky_display = InkyPHAT("yellow")
-inky_display.set_border(inky_display.WHITE)
+inky_display = auto()
+
 img = Image.new('P', (inky_display.WIDTH, inky_display.HEIGHT))
+
+display.windvane(img=img)
+inky_display.set_image(img)
+inky_display.show()
